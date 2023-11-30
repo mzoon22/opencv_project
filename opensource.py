3018f7c3765b61322ea58d4de9fac4334de1c047
mzoon22 <144039850+mzoon22@users.noreply.github.com>
2023-11-30 19:55:13 +0900

    Delete opensource.py

--- opensource.py
+++
@@ -1,7 +0,0 @@
-total 940K
-drwxr-xr-x 1 mzoon22 197121    0 Nov 30 19:34 ./
-drwxr-xr-x 1 mzoon22 197121    0 Nov 30 18:51 ../
-drwxr-xr-x 1 mzoon22 197121    0 Nov 30 19:36 .git/
--rw-r--r-- 1 mzoon22 197121 909K Nov 30 17:14 haar_face.xml
--rw-r--r-- 1 mzoon22 197121  511 Nov 30 18:36 haar_mouth.xml
--rw-r--r-- 1 mzoon22 197121    0 Nov 30 19:36 opensource.py
